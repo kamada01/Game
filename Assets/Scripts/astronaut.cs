69c3150978265fd657c8bcd46b1ebc12822e44a5
kamada01 <ada.kamym@gmail.com>
2023-04-11 02:27:20 +0800

    Added astronaut png and bg, astronaut basic movement

---
+++ Assets/Scripts/astronaut.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Astronaut : MonoBehaviour
+{
+    public float movementSpeed;
+    public Rigidbody2D rb;
+    Vector2 movement;
+
+    public Camera cam;
+    Vector2 mousePosition; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // get movement input
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        // get mouse position for aiming
+        mousePosition = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    private void FixedUpdate()
+    {
+        // update player position
+        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);
+
+        // rotate player according to mouse position
+        Vector2 direction = mousePosition - rb.position;
+        float rotation = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = rotation;
+    }
+}
