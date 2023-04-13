92f44eb6b231dc1ed9fb6481e74bff4cda892e4b
kamada01 <ada.kamym@gmail.com>
2023-04-13 12:27:58 +0800

    Testing gun

--- Assets/Scripts/ItemSelectHandler.cs
+++ Assets/Scripts/ItemSelectHandler.cs
@@ -7,6 +7,18 @@ public class ItemSelectHandler : MonoBehaviour
     public Inventory _Inventory;
     public void OnItemSelected()
     {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2)){
 
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3)) {
+
+
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4)) {
+
+        }
     }
 }
